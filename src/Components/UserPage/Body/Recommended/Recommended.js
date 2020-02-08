84e810418d2d9e95f2f48d022eeae43a05a2057a
stormfireuttam <40880896+stormfireuttam@users.noreply.github.com>
2020-02-08 13:48:03 +0530

    Add files via upload

---
+++ src/Components/UserPage/Body/Recommended/Recommended.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Junior from '../../../../UI/JuniorSlider/Junior';
+import './Recommended.css';
+
+const Recommended = () => {
+    return(
+        <React.Fragment>
+            <h3 style={{marginLeft:'18px'}}>Recommended For You</h3><br/>
+            <Junior/>
+        </React.Fragment>
+        
+    );
+}
+
+export default Recommended;
