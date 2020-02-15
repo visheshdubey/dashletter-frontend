c2652d7b2b2336cb052ec4e64318ca06e748984a
stormfireuttam <40880896+stormfireuttam@users.noreply.github.com>
2020-02-15 18:29:21 +0530

    Add files via upload

--- src/Components/UserPage/Toolbar/Toolbar.js
+++ src/Components/UserPage/Toolbar/Toolbar.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {Navbar, Nav, InputGroup, FormControl} from 'react-bootstrap';
+import {Navbar, InputGroup, FormControl} from 'react-bootstrap';
 import './Toolbar.css';
 
 class Toolbar extends React.Component {
     render() {
         return(
             <Navbar sticky="top" expand="lg" className="bar">
-                <Navbar.Brand href="#home" className="ml-4 mr-5 my-auto" as="h1">Dashletter</Navbar.Brand>
+                <Navbar.Brand href="/" className="ml-4 mr-5"><h2><b>Dashletter</b></h2></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <InputGroup className="mx-5" size="sm">
                     <FormControl
-                            placeholder="Search"
+                            placeholder="Search..."
                             style={{backgroundColor: '#f2f2f2'}}/>   
                     </InputGroup>
-                    <Nav className="ml-5">
-                        <Link to ="/"><Nav.Link href="#home" className="mr-4">Home</Nav.Link></Link>
-                        <Nav.Link href="#link" className="mr-4">Notifications</Nav.Link>
-                        <Nav.Link href="#link" className="mr-4">{this.props.userName}</Nav.Link>
-                    </Nav>  
                 </Navbar.Collapse>
             </Navbar>
         );
--- src/Components/UserPage/UserPage.js
+++ src/Components/UserPage/UserPage.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import Toolbar from './Toolbar/Toolbar';
 import Body from './Body/Body';
+import './Userpage.scss';
+import {NavLink} from 'react-router-dom';
+import {Button} from 'react-bootstrap';
 
 class UserPage extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <Toolbar userName="Vishesh"/>
-                <br/>
-                <Body />
+                <div class="wrapper">
+                    <div class="sidebar">
+                        <h5>Osama Abrar</h5>
+                        <ul>
+                            <div activeClassName="active"><li><NavLink exact to="/user/inbox"><i class="fas fa-envelope-open-text"></i>INBOX</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user"><i className="fas fa-home"></i>Home</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/Favourites"><i className="fas fa-star"></i>Favourites</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/Trending"><i className="fas fa-fire"></i>Trending</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/Subscribed"><i className="fas fa-atlas"></i>Subscribed</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/Settings"><i className="fas fa-users-cog"></i>Settings</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/Create">Create a Team</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/Folders"><i className="fas fa-folder"></i>Folders</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/Trash"><i className="fas fa-trash-alt"></i>Trash</NavLink></li></div>
+                        </ul> 
+                    </div>
+                    <div class="main_content">
+                       <Body/>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
