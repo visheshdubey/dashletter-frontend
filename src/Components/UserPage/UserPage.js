39342cda939b05e93bba497d0fcb5905ce607ead
stormfireuttam <40880896+stormfireuttam@users.noreply.github.com>
2020-02-16 22:25:48 +0530

    Add files via upload

--- src/Components/UserPage/UserPage.js
+++ src/Components/UserPage/UserPage.js
@@ -2,31 +2,39 @@ import React from 'react';
 import Toolbar from './Toolbar/Toolbar';
 import Body from './Body/Body';
 import './Userpage.scss';
-import {NavLink} from 'react-router-dom';
+import Inbox from '../Inbox/Inbox';
+import {BrowserRouter as Router, NavLink, Route} from 'react-router-dom';
 
 class UserPage extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Toolbar userName="Vishesh"/>
+                <Toolbar/>
                 <div class="wrapper">
+                <Router>
                     <div class="sidebar">
                         <h5>Osama Abrar</h5>
                         <ul>
                             <div activeClassName="active"><li><NavLink exact to="/user/inbox"><i class="fas fa-envelope-open-text"></i>INBOX</NavLink></li></div>
                             <div activeClassName="active"><li><NavLink exact to="/user"><i className="fas fa-home"></i>Home</NavLink></li></div>
-                            <div activeClassName="active"><li><NavLink exact to="/user/Favourites"><i className="fas fa-star"></i>Favourites</NavLink></li></div>
-                            <div activeClassName="active"><li><NavLink exact to="/user/Trending"><i className="fas fa-fire"></i>Trending</NavLink></li></div>
-                            <div activeClassName="active"><li><NavLink exact to="/user/Subscribed"><i className="fas fa-atlas"></i>Subscribed</NavLink></li></div>
-                            <div activeClassName="active"><li><NavLink exact to="/user/Settings"><i className="fas fa-users-cog"></i>Settings</NavLink></li></div>
-                            <div activeClassName="active"><li><NavLink exact to="/user/Create">Create a Team</NavLink></li></div>
-                            <div activeClassName="active"><li><NavLink exact to="/user/Folders"><i className="fas fa-folder"></i>Folders</NavLink></li></div>
-                            <div activeClassName="active"><li><NavLink exact to="/user/Trash"><i className="fas fa-trash-alt"></i>Trash</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/favourites"><i className="fas fa-star"></i>Favourites</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/trending"><i className="fas fa-fire"></i>Trending</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/subscribed"><i className="fas fa-atlas"></i>Subscribed</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/settings"><i className="fas fa-users-cog"></i>Settings</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/create">Create a Team</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/folders"><i className="fas fa-folder"></i>Folders</NavLink></li></div>
+                            <div activeClassName="active"><li><NavLink exact to="/user/trash"><i className="fas fa-trash-alt"></i>Trash</NavLink></li></div>
                         </ul> 
                     </div>
                     <div class="main_content">
-                       <Body/>
+                        <Route exact path="/user" component={Body}/>
+                        <Route path="/user/inbox" render={() => <Inbox title="Inbox"/>}/>  
+                        <Route path="/user/favourites" render={() => <Inbox title="Favourites"/>}/>  
+                        <Route path="/user/trending" render={() => <Inbox title="Trending"/>}/>  
+                        <Route path="/user/subscribed" render={() => <Inbox title="Subscribed"/>}/>  
+                        <Route path="/user/create" render={() => <Inbox title="Create"/>}/>            
                     </div>
+                    </Router>
                 </div>
             </React.Fragment>
         );
