7798d884c1b72243d575ec4f6b019eab0ea84af5
stormfireuttam <40880896+stormfireuttam@users.noreply.github.com>
2020-03-22 12:47:11 +0530

    Add files via upload

--- src/Components/LoginForm/Login.js
+++ src/Components/LoginForm/Login.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import {Button, Form, Container} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
+import Axios from 'axios';
 
-const Login = () => (
-    <React.Fragment>
+class Login extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            message: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id] : e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const {email, password} = this.state
+        try {
+            const token = Axios.post('https://dashletter-backend.herokuapp.com/login', {email,password})
+            localStorage.setItem("token", token)
+            this.setState({
+                loggedIn: true
+            })
+        } catch (err) {
+            this.setState({
+                error: err.message
+            })
+        }
+        console.log(this.state)
+    }
+    render() {
+       return(
         <Container className="containBlock">
             <h1>Dashletter</h1>
-            <Form className="formblock">
-                <Form.Group  controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Enter Password" />
-                </Form.Group>
-                <Button variant="danger" type="submit" block>
-                    Login
-                </Button>
-            </Form>         
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label forhtml="email">Email </label>
+                    <input type="email" className="form-control" placeholder="Enter email" onChange={this.handleChange} id="email"/>
+                </div>
+                <div className="form-group">
+                    <label forhtml="pwd">Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" id="password" onChange={this.handleChange}/>
+                </div><br/>
+                <button type="submit" className="btn btn-danger btn-block">Submit</button>
+                </form> 
         </Container>      
-    </React.Fragment>
-)
+
+       );
+   }
+}
 
 export default Login;
--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -1,43 +1,71 @@
 import React from 'react';
 import './SignUpForm.css';
-import {Container, Form, Button, ButtonGroup} from 'react-bootstrap';
+import {Container, Button, ButtonGroup} from 'react-bootstrap';
+import Axios from 'axios';
 
-const SignUp = () => (
-        <React.Fragment>
-            <Container className="containBlock">
-            <h1>
-                Welcome to Dashletter
-            </h1>
-            <Form className="formblock">
-            <Form.Group  controlId="formBasicEmail">
-                <Form.Control size="sm" type="email" placeholder="Enter email" />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-                <Form.Control size="sm" type="password" placeholder="Enter Password" />
-            </Form.Group>
-            <Form.Group controlId="ConfirmPassword">
-                <Form.Control size="sm" type="password" placeholder="Confirm Password" />
-            </Form.Group>
-            <Form.Group controlId="Age">
-                <Form.Control size="sm" type="number" placeholder="Enter your Age" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox" className="formend">
-                <Form.Check type="checkbox" size="sm" label="Agree to the terms and conditions" />
-            </Form.Group>
-            <Button variant="danger" type="submit" block>
-                Submit
-            </Button>
-        </Form>    
-        <hr/>    
-        <div className="d-flex flex-column">
-        <ButtonGroup>
-            <Button variant="danger" className="googlelogo">Login with Google+</Button>
-            <Button className="fblogo">Login with Facebook</Button>
-        </ButtonGroup>
-        </div> 
-    </Container>    
-</React.Fragment>
+class SignUp extends React.Component {
+    constructor(props){
+        super(props)
+        let loggedIn = false
+        this.state = {
+            email: '',
+            password: '',
+            error: '',
+            loggedIn
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id] : e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        const {email, password} = this.state
+        try {
+            const token = Axios.post('https://dashletter-backend.herokuapp.com/signup', {email,password})
+            localStorage.setItem("token", token)
+            this.setState({
+                loggedIn: true
+            })
+        } catch (err) {
+            this.setState({
+                error: err.message
+            })
+        }
+    }
+    render() {
+        return(
+            <div>
+            <h1><b>Welcome to Dashletter</b></h1>
+            <Container>
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label forhtml="email">Email </label>
+                    <input type="email" className="form-control" placeholder="Enter email" onChange={this.handleChange} id="email"/>
+                </div>
+                <div className="form-group">
+                    <label forhtml="pwd">Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" id="password" onChange={this.handleChange}/>
+                </div>
+                <div className="form-group">
+                    <label forhtml="pwd">Confirm Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" id="password" onChange={this.handleChange}/>
+                </div>
+                <button type="submit" className="btn btn-danger btn-block">Submit</button>
+            </form> 
+            <hr/>    
+            <div className="d-flex flex-column">
+            <ButtonGroup>
+                <Button variant="danger" className="googlelogo">Login with Google+</Button>
+                <Button className="fblogo">Login with Facebook</Button>
+            </ButtonGroup>
+            </div> 
+        </Container>  
+        </div>  
+        );
+    }
+}
         
-)
-
 export default SignUp;
